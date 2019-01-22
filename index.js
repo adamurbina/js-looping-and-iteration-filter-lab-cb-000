13719967f28ccc9cc2286a4dca2d21fd1328dae8
Adam Urbina <adamurbina@gmail.com>
2019-01-22 21:30:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function findMatching(collection, targetName) {
 
-  function compareStrings(string1, string2) {
-    return string1.toUpperCase() === string2.toUpperCase();
-  }
-
-  const result = collection.filter(item => compareStrings(item, targetName));
+  // function compareStrings(string1, string2) {
+  //   return string1.toUpperCase() === string2.toUpperCase();
+  // }
+  // 
+  // const result = collection.filter(item => compareStrings(item, targetName));
 
   const result = collection.filter(item => item.toUpperCase() === targetName.toUpperCase());
 
