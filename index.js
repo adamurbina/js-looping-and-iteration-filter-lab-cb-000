56eebdbf2d9e54a99308ed117c1445a8e8d46bb0
Adam Urbina <adamurbina@gmail.com>
2019-01-22 21:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 function findMatching(collection, targetName) {
-
   return collection.filter(item => item.toUpperCase() === targetName.toUpperCase());
-
 }
 
 function fuzzyMatch(collection, text) {
-
   return collection.filter(item => text === item.slice(0,text.length));
 }
 
